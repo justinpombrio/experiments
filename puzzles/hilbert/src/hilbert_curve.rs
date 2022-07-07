72af682f14e8956228084f85d3c9d2c2f2a775fb
Justin Pombrio <justin@geopi.pe>
2022-07-07 01:55:48 -0400

    Hilbert Curve puzzle

---
+++ puzzles/hilbert/src/hilbert_curve.rs
@@ -0,0 +1,81 @@
+use std::mem;
+
+// Algorithm from Wikipedia
+
+pub struct HilbertCurve {
+    dimension: u32,
+}
+
+impl HilbertCurve {
+    /// Create a new Hilbert curve of size `dimension x dimension`.
+    pub fn new(dimension: u32) -> HilbertCurve {
+        // Check that dimension is a power of 2.
+        assert!(dimension & (dimension - 1) == 0, "Hilbert curve dimension must be power of 2");
+        HilbertCurve {
+            dimension,
+        }
+    }
+
+    /// (x, y) point -> distance along the curve.
+    #[allow(unused)]
+    pub fn point_to_dist(&self, point: (u32, u32)) -> u32 {
+        let (mut x, mut y) = point;
+        let mut d = 0;
+        let mut s = self.dimension / 2;
+        while s > 0 {
+            let rx = ((x & s) > 0) as u32;
+            let ry = ((y & s) > 0) as u32;
+            d += s * s * ((3 * rx) ^ ry);
+            rotate(self.dimension, &mut x, &mut y, rx, ry);
+            s /= 2;
+        }
+        d
+    }
+
+    /// Distance along the curve -> (x, y) point.
+    pub fn dist_to_point(&self, d: u32) -> (u32, u32) {
+        let mut t = d;
+        let (mut x, mut y) = (0, 0);
+        let mut s = 1;
+        while s < self.dimension {
+            let rx = 1 & (t / 2);
+            let ry = 1 & (t ^ rx);
+            rotate(s, &mut x, &mut y, rx, ry);
+            x += s * rx;
+            y += s * ry;
+            t /= 4;
+            s *= 2;
+        }
+        (x, y)
+    }
+
+    /// The number of points in the curve.
+    pub fn length(&self) -> u32 {
+        self.dimension * self.dimension
+    }
+}
+
+fn rotate(n: u32, x: &mut u32, y: &mut u32, rx: u32, ry: u32) {
+    if ry == 0 {
+        if rx == 1 {
+            *x = n.wrapping_sub(1).wrapping_sub(*x);
+            *y = n.wrapping_sub(1).wrapping_sub(*y);
+        }
+        mem::swap(x, y);
+    }
+}
+
+#[test]
+fn test_hilbert_curve() {
+    for n in [1, 2, 4, 8, 16, 32] {
+        let curve = HilbertCurve::new(n);
+        for d in 0..n * n {
+            assert_eq!(curve.point_to_dist(curve.dist_to_point(d)), d);
+        }
+        for x in 0..n {
+            for y in 0..n {
+                assert_eq!(curve.dist_to_point(curve.point_to_dist((x, y))), (x, y));
+            }
+        }
+    }
+}
---
+++ puzzles/hilbert/src/oklab.rs
@@ -0,0 +1,96 @@
+/// Convert from the OKLAB color space to srgb. Returns an srgb color,
+/// and a boolean indicating whether the color was clamped (whether it
+/// was out of bounds).
+pub fn oklab_to_srgb(mut lab: [f32; 3]) -> ([u8; 3], bool) {
+    // A hack so that L=1 is pure white RGB
+    lab[0] *= 1.04;
+
+    let l = lab[0] + 0.3963377774 * lab[1] + 0.2158037573 * lab[2];
+    let m = lab[0] - 0.1055613458 * lab[1] - 0.0638541728 * lab[2];
+    let s = lab[0] - 0.0894841775 * lab[1] - 1.2914855480 * lab[2];
+
+    let l = l * l * l;
+    let m = m * m * m;
+    let s = s * s * s;
+
+    let r = 4.0767416621 * l - 3.3077115913 * m + 0.2309699292 * s;
+    let g = -1.2684380046 * l + 2.6097574011 * m - 0.3413193965 * s;
+    let b = -0.0041960863 * l - 0.7034186147 * m + 1.7076147010 * s;
+
+    let (r, clamped_r) = to_gamma(r);
+    let (g, clamped_g) = to_gamma(g);
+    let (b, clamped_b) = to_gamma(b);
+
+    ([r, g, b], clamped_r || clamped_g || clamped_b)
+}
+
+fn to_gamma(u: f32) -> (u8, bool) {
+    let g = if u >= 0.0031308 {
+        1.005 * u.powf(1.0 / 2.4) - 0.055
+    } else {
+        12.92 * u
+    };
+    let component = (g * 255.0).round();
+    if component < 0.0 {
+        return (0, true);
+    }
+    if component > 255.0 {
+        return (255, true);
+    }
+    (component as u8, false)
+}
+
+#[test]
+fn test_oklab() {
+    /*
+    // L / max valid (a,b)-radius
+    // 0	0.034
+    // 0.05	0.02
+    // 0.1	0.02
+    // 0.15	0.027
+    // 0.2	0.036
+    // 0.25	0.044
+    // 0.3	0.053
+    // 0.35	0.062
+    // 0.4	0.07
+    // 0.45	0.079
+    // 0.5	0.088
+    // 0.55	0.097
+    // 0.6	0.106
+    // 0.65	0.114
+    // 0.7	0.123
+    // 0.75	0.132
+    // 0.8	0.104
+    // 0.85	0.077
+    // 0.9	0.05
+    // 0.95	0.025
+    // 1	0
+    fn radius(l: f32) -> f32 {
+        const PI: f32 = std::f32::consts::PI;
+
+        for r in (0..1000).map(|n| (n as f32) / 1000.0) {
+            for angle in (0..360).map(|n| 2.0 * PI * (n as f32) / 360.0) {
+                let a = r * angle.cos();
+                let b = r * angle.sin();
+                let (_, clamped) = oklab_to_srgb([l, a, b]);
+                if clamped {
+                    return ((r * 1000.0) - 1.0) / 1000.0;
+                }
+            }
+        }
+        unreachable!();
+    }
+    for l in (0..21).map(|n| (n as f32) / 20.0) {
+        println!("{}\t{}", l, radius(l));
+    }
+    assert!(false);
+    */
+
+    assert_eq!(oklab_to_srgb([0.0, 0.0, 0.0]), ([0, 0, 0], false));
+    assert_eq!(oklab_to_srgb([1.0, 0.0, 0.0]), ([255, 255, 255], false));
+    assert_eq!(oklab_to_srgb([0.5, 0.0, 0.0]), ([99, 99, 99], false));
+    assert_eq!(oklab_to_srgb([0.75, 0.13, 0.0]), ([238, 139, 171], false));
+    assert_eq!(oklab_to_srgb([0.75, -0.13, 0.0]), ([51, 199, 177], false));
+    assert_eq!(oklab_to_srgb([0.75, 0.0, 0.13]), ([205, 170, 69], false));
+    assert_eq!(oklab_to_srgb([0.75, 0.0, -0.13]), ([143, 169, 253], false));
+}
