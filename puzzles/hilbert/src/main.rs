3f17d041b5903b5eca7d58bce4b66a7d94257b24
Justin Pombrio <justin@geopi.pe>
2022-07-08 23:08:01 -0400

    More parameters, maximally pretty picture

--- puzzles/hilbert/src/main.rs
+++ puzzles/hilbert/src/main.rs
@@ -7,11 +7,18 @@ use oklab::oklab_to_srgb;
 use std::mem;
 
 // OkLab Colors. All angles are measured in turns. Obviously.
-const START_COLOR_ANGLE: f64 = 0.0;
+const START_COLOR_ANGLE: f64 = 1.0;
 const TOTAL_COLOR_ANGLE: f64 = 1.0;
+const MIN_SHADE: f64 = 0.3;
+const NUM_SHADE_TRANSITIONS: f64 = 1.5;
+const MAX_LIGHTNESS: f64 = 0.7;
+const MAX_SATURATION: f64 = 0.123;
+const BORDER_COLOR: [u16; 3] = [u16::MAX/3, u16::MAX/3, u16::MAX/3];
+const HACKY_SAT_MULTIPLIER: f64 = 1.2;
 
 // Dimensions
 const IMAGE_SIZE: u32 = 4096;
+const BORDER_SIZE: u32 = 32;
 const HILBERT_SIZE: u32 = 256;
 const CELL_WIDTH: u32 = IMAGE_SIZE / HILBERT_SIZE;
 
@@ -21,31 +28,38 @@ const DRAW_SQUARES: bool = true;
 // Curve
 const DRAW_CURVE: bool = false;
 const LINE_WIDTH: u32 = 7 * CELL_WIDTH / 8;
-const USE_FIXED_CURVE_COLOR: bool = false;
-const CURVE_COLOR: [u16; 3] = [30, 30, 30];
+const USE_FIXED_CURVE_COLOR: bool = true;
+const CURVE_COLOR: [u16; 3] = [30 * 256, 30 * 256, 30 * 256];
 const ENDPOINT_BORDER: u32 = CELL_WIDTH / 16;
 
 // Checkerboard
 const DRAW_CHECKERBOARD: bool = false;
-const FOREGROUND_COLOR: [u16; 3] = [0, 0, 0];
-const BACKGROUND_COLOR: [u16; 3] = [255, 255, 255];
+const FOREGROUND_COLOR: [u16; 3] = [u16::MAX, u16::MAX, u16::MAX];
+const BACKGROUND_COLOR: [u16; 3] = [0, 0, 0];
 
 type Image = ImageBuffer<Rgb<u16>, Vec<u16>>;
 type Color = [u16; 3];
 
 pub fn main() {
-    let mut img = ImageBuffer::<Rgb<u16>, _>::new(IMAGE_SIZE, IMAGE_SIZE);
+    let mut img = ImageBuffer::<Rgb<u16>, _>::new(
+        IMAGE_SIZE + 2 * BORDER_SIZE,
+        IMAGE_SIZE + 2 * BORDER_SIZE,
+    );
 
     // Draw background
     for pixel in img.pixels_mut() {
-        pixel.0 = BACKGROUND_COLOR;
+        pixel.0 = BORDER_COLOR;
     }
     if DRAW_CHECKERBOARD {
         for (x, y, pixel) in img.enumerate_pixels_mut() {
+            let x = x + BORDER_SIZE;
+            let y = y + BORDER_SIZE;
             let sx = x / CELL_WIDTH / 8;
             let sy = y / CELL_WIDTH / 8;
             if (sx + sy) % 2 == 0 {
                 pixel.0 = FOREGROUND_COLOR;
+            } else {
+                pixel.0 = BACKGROUND_COLOR;
             }
         }
     }
@@ -62,7 +76,7 @@ pub fn main() {
         for d in 0..curve.length() {
             let point = scale(curve.dist_to_point(d));
             let frac = (d as f64) / (curve.length() as f64);
-            let (color, clamped) = colorscale(frac, 0.0, 1.0);
+            let (color, clamped) = colorscale(frac);
             total_colors += 1;
             clamped_colors += clamped as u32;
             let lower_left = (point.0 - CELL_WIDTH / 2, point.1 - CELL_WIDTH / 2);
@@ -83,19 +97,21 @@ pub fn main() {
             let color = if USE_FIXED_CURVE_COLOR {
                 CURVE_COLOR
             } else {
-                colorscale(frac, 0.0, 1.0).0
+                colorscale(frac).0
             };
             draw_segment(&mut img, start, middle, end, LINE_WIDTH / 2, color);
         }
     }
 
     // Draw start and end points
-    let start_color = colorscale(0.0, 0.0, 1.0).0;
-    let end_color = colorscale(1.0, 0.0, 1.0).0;
+    let start_color = colorscale(0.0).0;
+    let end_color = colorscale(1.0).0;
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         let width = CELL_WIDTH - ENDPOINT_BORDER;
         let border = ENDPOINT_BORDER;
-        let flipped_x = IMAGE_SIZE - x - 1;
+        let flipped_x = IMAGE_SIZE + 2 * BORDER_SIZE - x - 1;
+        let x = x + BORDER_SIZE;
+        let y = y + BORDER_SIZE;
         if x < width && x >= border && y < width && y >= border {
             pixel.0 = start_color;
         }
@@ -117,7 +133,7 @@ fn draw_rect(img: &mut Image, mut lower_left: (u32, u32), mut upper_right: (u32,
     }
     for x in lower_left.0..upper_right.0 {
         for y in lower_left.1..upper_right.1 {
-            img.get_pixel_mut(x, y).0 = color;
+            img.get_pixel_mut(x + BORDER_SIZE, y + BORDER_SIZE).0 = color;
         }
     }
 }
@@ -155,14 +171,19 @@ fn draw_segment(
     }
 }
 
-fn colorscale(f: f64, offset: f64, saturation: f64) -> (Color, bool) {
+fn colorscale(f: f64) -> (Color, bool) {
     const PI: f64 = std::f64::consts::PI;
 
-    let angle = START_COLOR_ANGLE + TOTAL_COLOR_ANGLE * f + offset;
-    let scale = (2.0 * f - 1.0).abs() * 0.8 + 0.2; // prev 0.9, sqrt
-    let rad = 0.15 * scale * saturation;
-    let l = 0.85 * scale;
-    let a = rad * (2.0 * PI * angle).sin();
-    let b = rad * (2.0 * PI * angle).cos();
+    let angle = START_COLOR_ANGLE + TOTAL_COLOR_ANGLE * f;
+    let transition = f % (1.0 / NUM_SHADE_TRANSITIONS) * NUM_SHADE_TRANSITIONS;
+    let mut raw_shade = (2.0 * transition - 1.0).abs();
+    if f < 1.0/3.0 {
+        raw_shade *= HACKY_SAT_MULTIPLIER;
+    }
+    let shade = raw_shade * (1.0 - MIN_SHADE) + MIN_SHADE;
+    let saturation = MAX_SATURATION * shade;
+    let l = MAX_LIGHTNESS * shade;
+    let a = saturation * (2.0 * PI * angle).sin();
+    let b = saturation * (2.0 * PI * angle).cos();
     oklab_to_srgb([l, a, b])
 }
